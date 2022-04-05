98cc8f6399315c6e2773a670b9dbb2365c365dab
Kuh <emkukuh@gmail.com>
2022-04-06 05:21:57 +0700

    wip

--- TipJar/Constants/Spaces.swift
+++ TipJar/Constants/Spaces.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftUI
 
 struct Spaces {
+    static let value10: CGFloat = 10
     static let value16: CGFloat = 16
     static let value22: CGFloat = 22
     static let value24: CGFloat = 24
