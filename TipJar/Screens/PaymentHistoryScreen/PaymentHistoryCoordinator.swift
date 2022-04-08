9eabd9987bd028f2af62dfe37de31ee46c8ec61f
Kuh <emkukuh@gmail.com>
2022-04-09 05:05:12 +0700

    wip

--- TipJar/Screens/PaymentHistoryScreen/PaymentHistoryCoordinator.swift
+++ TipJar/Screens/PaymentHistoryScreen/PaymentHistoryCoordinator.swift
@@ -10,19 +10,6 @@ import SwiftUI
 struct PaymentHistoryCoordinator: View {
     @Binding var activeCoordinatorName: String?
     var body: some View {
-//        NavigationView {
-//            ZStack {
-//                Screen { PaymentHistoryScreen(viewModel: createViewModel()) }
-//                    .navigationBarTitleDisplayMode(.inline)
-//                    .toolbar {
-//                        Button(action: { print("") }) {
-//                            Image(R.image.history.name)
-//                        }
-//                    }
-//            }
-//        }
-//        .background(Color.white)
-//        .overlay(NavigationBarView())
         NavigationLink(
             tag: PaymentHistoryCoordinator.name,
             selection: $activeCoordinatorName,
